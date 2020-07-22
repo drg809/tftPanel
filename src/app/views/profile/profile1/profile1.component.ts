77d4edbf1cde56988464da34b4e565415417a0c5
Daniel Rodriguez <drg809@gmail.com>
2020-07-22 21:11:23 +0200

    customize navbar:

--- src/app/views/profile/profile1/profile1.component.ts
+++ src/app/views/profile/profile1/profile1.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { SummonerService } from 'src/app/shared/services/summoners.service';
+import { Summoner } from 'src/app/shared/models/summoner';
 
 @Component({
   selector: 'app-profile1',
@@ -6,56 +8,15 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./profile1.component.scss']
 })
 export class Profile1Component implements OnInit {
-  imagesBasic = [
-    {
-      img: 'https://mdbootstrap.com/img/Photos/Horizontal/Nature/12-col/img%20(117).jpg',
-      thumb: 'https://mdbootstrap.com/img/Photos/Horizontal/Nature/12-col/img%20(117).jpg',
-      description: 'Image 1'
-    },
-    {
-      img: 'https://mdbootstrap.com/img/Photos/Horizontal/Nature/12-col/img%20(98).jpg',
-      thumb: 'https://mdbootstrap.com/img/Photos/Horizontal/Nature/12-col/img%20(98).jpg',
-      description: 'Image 2'
-    },
-    {
-      img: 'https://mdbootstrap.com/img/Photos/Horizontal/Nature/12-col/img%20(131).jpg',
-      thumb: 'https://mdbootstrap.com/img/Photos/Horizontal/Nature/12-col/img%20(131).jpg',
-      description: 'Image 3'
-    },
-    {
-      img: 'https://mdbootstrap.com/img/Photos/Horizontal/Nature/12-col/img%20(123).jpg',
-      thumb: 'https://mdbootstrap.com/img/Photos/Horizontal/Nature/12-col/img%20(123).jpg',
-      description: 'Image 4'
-    },
-    {
-      img: 'https://mdbootstrap.com/img/Photos/Horizontal/Nature/12-col/img%20(118).jpg',
-      thumb: 'https://mdbootstrap.com/img/Photos/Horizontal/Nature/12-col/img%20(118).jpg',
-      description: 'Image 5'
-    },
-    {
-      img: 'https://mdbootstrap.com/img/Photos/Horizontal/Nature/12-col/img%20(128).jpg',
-      thumb: 'https://mdbootstrap.com/img/Photos/Horizontal/Nature/12-col/img%20(128).jpg',
-      description: 'Image 6'
-    },
-    {
-      img: 'https://mdbootstrap.com/img/Photos/Horizontal/Nature/12-col/img%20(132).jpg',
-      thumb: 'https://mdbootstrap.com/img/Photos/Horizontal/Nature/12-col/img%20(132).jpg',
-      description: 'Image 7'
-    },
-    {
-      img: 'https://mdbootstrap.com/img/Photos/Horizontal/Nature/12-col/img%20(115).jpg',
-      thumb: 'https://mdbootstrap.com/img/Photos/Horizontal/Nature/12-col/img%20(115).jpg',
-      description: 'Image 8'
-    },
-    {
-      img: 'https://mdbootstrap.com/img/Photos/Horizontal/Nature/12-col/img%20(133).jpg',
-      thumb: 'https://mdbootstrap.com/img/Photos/Horizontal/Nature/12-col/img%20(133).jpg',
-      description: 'Image 9'
-    }
-];
-  constructor() { }
+  summoner: string;
+  constructor(private summonerService: SummonerService) { }
 
   ngOnInit() {
   }
 
+  newSummoner() {
+    const summoner: Summoner = {userId: '5f15a1daf7bfa346b272c89e', summonerName: this.summoner};
+    this.summoner != '' ? this.summonerService.create(summoner) : alert('Introduce tu nombre de invocador');
+
+  }
 }
