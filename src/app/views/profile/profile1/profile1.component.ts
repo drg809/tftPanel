abaf36b1a6cb91bf0679ccc9bf2c4ac83e8605d6
Daniel Rodriguez <drg809@gmail.com>
2020-07-28 01:21:42 +0200

    name changed to fix with api

--- src/app/views/profile/profile1/profile1.component.ts
+++ src/app/views/profile/profile1/profile1.component.ts
@@ -29,7 +29,7 @@ export class Profile1Component implements OnInit {
     this.summonerService.getMatches(userId).subscribe((emitData: any) => { console.log(emitData); });
   }
 
-  getLastMatch(userId: any) {
+  setLastMatch(userId: any) {
     this.summonerService.setLastMatchInfo(userId).subscribe((emitData: any) => { console.log(emitData); });
   }
 
