881b5e915b97be9d419fac5ee2657d95278f7006
Daniel Rodriguez <drg809@gmail.com>
2020-07-28 10:15:29 +0200

    model fixed to nested order

--- src/app/shared/models/match.ts
+++ src/app/shared/models/match.ts
@@ -2,5 +2,13 @@ export class SumMatch {
   public id?: string;
   public userId!: string | null;
   public entrie?: string | null;
-  public data?: object | null;
+  public data: { metadata: object, info: {
+    game_datetime: number,
+    game_length: number,
+    game_variation: string,
+    game_version: string,
+    participants: object,
+    queue_id: number,
+    tft_set_number: number
+  } };
 }
