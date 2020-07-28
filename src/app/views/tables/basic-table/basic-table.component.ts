ebd8c12bc494ffac37c6ea5c1c60337f377c1e9b
Daniel Rodriguez <drg809@gmail.com>
2020-07-28 08:02:08 +0200

    swap table structure to a div structure

--- src/app/views/tables/basic-table/basic-table.component.ts
+++ src/app/views/tables/basic-table/basic-table.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { SummonerService } from 'src/app/shared/services/summoners.service';
+import { SumMatch } from 'src/app/shared/models/match';
+import { User } from 'src/app/shared/models/user';
 
 @Component({
   selector: 'app-basic-table',
@@ -6,10 +9,18 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./basic-table.component.scss']
 })
 export class BasicTableComponent implements OnInit {
+  match: SumMatch;
+  user: User;
 
-  constructor() { }
+  constructor(private summonerService: SummonerService) { }
 
   ngOnInit() {
+    this.user = JSON.parse(localStorage.getItem('currentUser') || '{}');
+    console.log(this.user._id);
+    this.summonerService.getHistory(this.user._id).subscribe(data => {
+      this.match = data;
+      console.log(this.match);
+    });
   }
 
 }
