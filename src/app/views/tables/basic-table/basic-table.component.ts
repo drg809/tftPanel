ef6c361f966b7b81ba55880203b737ca9f7f6eb5
Daniel Rodriguez <drg809@gmail.com>
2020-07-29 06:47:16 +0200

    basic-table restored

--- src/app/views/tables/basic-table/basic-table.component.ts
+++ src/app/views/tables/basic-table/basic-table.component.ts
@@ -1,7 +1,4 @@
 import { Component, OnInit } from '@angular/core';
-import { SummonerService } from 'src/app/shared/services/summoners.service';
-import { SumMatch } from 'src/app/shared/models/match';
-import { User } from 'src/app/shared/models/user';
 
 @Component({
   selector: 'app-basic-table',
@@ -9,18 +6,8 @@ import { User } from 'src/app/shared/models/user';
   styleUrls: ['./basic-table.component.scss']
 })
 export class BasicTableComponent implements OnInit {
-  matchs: SumMatch[];
-  user: User;
-
-  constructor(private summonerService: SummonerService) { }
+  constructor() { }
 
   ngOnInit() {
-    this.user = JSON.parse(localStorage.getItem('currentUser') || '{}');
-    console.log(this.user._id);
-    this.summonerService.getMatchesHistoric(this.user._id).subscribe(data => {
-      this.matchs = data;
-      this.matchs.sort((a, b) => b.data?.info.game_datetime  - a.data?.info.game_datetime);
-      console.log(this.matchs);
-    });
   }
 }
