1324bbd28d859578fd55f35ff04b35be39832f75
Daniel Rodriguez <drg809@gmail.com>
2020-07-30 06:47:51 +0200

    navigate to match view when click in match

--- src/app/views/historical/historical.component.ts
+++ src/app/views/historical/historical.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { SummonerService } from 'src/app/shared/services/summoners.service';
 import { SumMatch } from 'src/app/shared/models/match';
 import { User } from 'src/app/shared/models/user';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-historical',
@@ -12,15 +13,20 @@ export class HistoricalComponent implements OnInit {
   matchs: SumMatch[];
   user: User;
 
-  constructor(private summonerService: SummonerService) { }
+  constructor(private summonerService: SummonerService,
+              private router: Router) { }
 
   ngOnInit() {
     this.user = JSON.parse(localStorage.getItem('currentUser') || '{}');
-    console.log(this.user._id);
     this.summonerService.getMatchesHistoric(this.user._id).subscribe(data => {
       this.matchs = data;
       this.matchs.sort((a, b) => b.data?.info.game_datetime  - a.data?.info.game_datetime);
       console.log(this.matchs);
     });
+  }
+
+  navigateToMatch(entrie: string) {
+    console.log(entrie);
+    this.router.navigate(['/historical/' + entrie]);
   }
 }
